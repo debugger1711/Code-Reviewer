cdfd3881563b5141dd8252a89892b1a3e7fd34da
Visha <visha@example.com>
2026-03-22 18:41:49 +0530

    Vercel configure

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
-# Ensure this points to your project's settings
+# 👇 Add project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codereviewer.settings")
 
-# Vercel requires the variable to be named 'app'
+from django.core.wsgi import get_wsgi_application
+
 app = get_wsgi_application()
